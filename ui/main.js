594e6e3352124cdbb25c88fa1853f099d391f383
nadodiganesh <30748128+nadodiganesh@users.noreply.github.com>
2017-08-27 22:41:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 //Mov img
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function () {
   img.style.marginLeft = '100px' ; 
 };
