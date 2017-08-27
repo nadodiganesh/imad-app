fae16cb62e8b4b689ee8fb908fe1dd8f9878aba6
nadodiganesh <30748128+nadodiganesh@users.noreply.github.com>
2017-08-27 23:09:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,10 @@
-console.log('Loaded!');
-
-//Mov img
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 2;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-  var interval = setInterval(moveRight, 50);
+var button = document.getElementByID('counter');
+var counter = 0;
+button.onclick = function () {
+  
+  //Make request to counter end point
+  
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
